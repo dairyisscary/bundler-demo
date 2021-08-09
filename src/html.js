39915c5445c887c23cc048fbcc413eefc1e91ea0
Eric Kim-Butler <eric@kimbutler.xyz>
2021-08-09 12:44:08 +0200

    Add Sanitizer

--- src/html.js
+++ src/html.js
@@ -1,8 +1,10 @@
+import sanitizeHTML from "sanitize-html";
+
 export function convertToNumber(input) {
   return Number(input.value.trim());
 }
 
 export function setOutputHtml(element, content) {
-  // WARNING XSS DO NOT DO IN PRODUCTION
-  element.innerHTML = `The result is <strong>${content}</strong>.`;
+  const sanitizedContent = sanitizeHTML(content);
+  element.innerHTML = `The result is <strong>${sanitizedContent}</strong>.`;
 }
