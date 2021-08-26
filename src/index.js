0f5f72851ff303f122641ff67de0ca3a1b9952af
Eric Kim-Butler <eric@kimbutler.xyz>
2021-08-26 13:36:05 +0200

    Add Calc Image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { convertToNumber, setOutputHtml } from "./html";
 import { add } from "./math";
+import CalculatorData from "./images/calculator.svg";
 import "./index.css";
 
 console.log("This my app!");
@@ -8,6 +9,13 @@ const [a] = document.getElementsByName("a");
 const [b] = document.getElementsByName("b");
 const form = document.getElementById("calc-form");
 const output = document.getElementById("output");
+const header = document.getElementById("header");
+
+const img = document.createElement("img");
+img.alt = "Calculator";
+img.src = CalculatorData;
+img.setAttribute("class", "ml-auto h-14 w-auto");
+header.appendChild(img);
 
 form.addEventListener("submit", (evt) => {
   evt.preventDefault();
